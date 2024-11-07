e9ae83a226fd868dbc38bc972985601a35e3f195
sergiosaraivajr <sergiosaraivajr01@gmail.com>
2024-11-07 13:07:10 -0300

    criação da page inicio

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,12 @@
+function Inicio() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold mb-4">Bem-vindo ao Início</h1>
+        <p className="text-gray-700">Este é o conteúdo da página Início.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Inicio;
